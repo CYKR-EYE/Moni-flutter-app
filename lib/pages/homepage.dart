125888733a4e97cc56cf6770ac6ea9bc3ba0e2fa
Paul Carouge <cykreye@gmail.com>
2025-04-10 15:26:49 +0200

    feat: update HomePage title and add body padding

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-// Page d'accueil avec un tableau de bord rapide
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Barre de navigation
       appBar: AppBar(
-        title: Text("Compte"),
+        title: Text("Accueil"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(22.0),
       ),
     );
   }
