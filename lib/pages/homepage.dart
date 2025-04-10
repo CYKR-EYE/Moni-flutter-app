4c9d70accfa0d322c3b1080aa876c6815f2c0a3b
Paul Carouge <cykreye@gmail.com>
2025-04-10 12:17:19 +0200

    refactor: remove unused homepage layout code

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,36 +5,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Moni - Budget Tracker')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Bienvenue sur Moni'),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the next screen
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                backgroundColor: Colors.orange,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              child: const Text(
-                'Commencer',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
