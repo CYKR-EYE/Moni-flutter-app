517f7ad4178b9674fab9c42c631f626fecc1a9a3
Paul Carouge <cykreye@gmail.com>
2025-04-10 13:34:12 +0200

    feat: update homepage layout and adjust SDK constraints

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+// Page d'accueil avec un tableau de bord rapide
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // Barre de navigation
+      appBar: AppBar(
+        title: Text("Compte"),
+      ),
+    );
   }
 }
